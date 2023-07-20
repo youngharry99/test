e27723736e9394164e57b6056dc56c70c464fe4e
YoungHarry <prodharry@qq.com>
2023-07-20 15:22:25 +0800

    'add test logic'

--- mqttSend.py
+++ mqttSend.py
@@ -13,14 +13,20 @@ paylad: 4,2,15
 请求车态数据
 
 订阅 U/JSON/BC66E100AC
+
+BC66E100AC
+B2D2E00075
+B4B2E0009E
 '''
 
 import paho.mqtt.client as mqtt
 import random
 import json
 import time
+import queue
 
-sn              = 'BC66E100AC'                                      #终端SN号
+#配置信息
+sn              = 'B2D2E00075'                                      #终端SN号
 broker          = 'mqtt-alpha.smart-iov.net'                        #测试网
 port            = 8801                                              #端口
 topic           = 'S' + sn                                          #平台发布Topic
@@ -34,6 +40,11 @@ userName        = 'test10'
 client_id       = "app_test10"
 mqttPassword    = "18cbd2b9800.aca5bc82c4ed053e9b6bed3a785e756a"
 '''
+INTERVAL_TIME   =   60
+RUN_TIME        =   20
+
+message_buff = queue.Queue()
+start_time = 0
 
 # 定义一个回调函数
 def on_connect(client, userdata, flags, rc):
@@ -42,7 +53,6 @@ def on_connect(client, userdata, flags, rc):
         print("Connect MQTT Server Success")
         #订阅终端发布主题
         client.subscribe(subTopic)
-
     elif rc == 4:
         print("User name or password wrong")
 
@@ -52,52 +62,92 @@ def on_subscribe(client, userdata, mid, grated_qos, properties=None):
 
 # 消息接收的回调函数
 def on_message(client, userdata, msg):
-    #print("Received message: " + str(msg.payload.decode()))
     payload = json.loads(msg.payload)
-    print("Received Message: " , payload)
+    #print("Received Message: " , payload)
+    try:
+        #解析数据
+        engine = payload['informations'][1]['value']['engine']
+        collect_time = payload['informations'][1]['value']['collect_time']
+        message_buff.put([engine, collect_time])
 
+    except Exception as result:
+        pass
+
+    finally:
+        if not message_buff.empty():
+                print('engine,stamp: ', message_buff.get())
+        #print("Received Message: " , message_buff.get())
+'''
+        global start_time
+        send_time = time.time()
+        elapsed_time = send_time - start_time
+        print(f"Message sent to IV100 in {elapsed_time} seconds")
+'''
 
 # 发布消息回调函数
 def on_publish(client, userdata, mid):
     print("Message Send To MQTT")
 
+
 #获取车辆上报数据
 def get_car_reporting_data():
     client.publish(topic, '4,2,15')     #车辆信息批量上报(0x0F)
-'''
-def publish_message():
-    # publish 5 messages to topic
-    paylad = '5,3,1677235643,' + str(random.randint(1000, 3000)) + ',34383038,C7'
-    client.publish(topic, paylad)
-    print("publish msg: " + paylad)
-    time.sleep(2)
-    requset_car_status_paylad = '4,2,15'
-    #请求车态数据
-    client.publish(topic, requset_car_status_paylad)
-    print("publish msg: " + requset_car_status_paylad)
-'''
+
+#发送主题消息
+def publish_message(des_Topic, cmd = 'C7'):
+    '''
+    publish messages to des_Topic
+    @des_Topic: 发布主题
+    @cmd:       命令,默认C7 鸣车
+    '''
+    paylad = '5,3,1677235643,' + str(random.randint(1000, 3000)) + ',34383038,' + cmd
+    client.publish(des_Topic, paylad)
+
+    send_time = time.time()
+    print('stamp:', send_time, " publish msg: " + paylad)
+
+
 def clinet_Init():
     client.username_pw_set(username=userName, password=mqttPassword)    #set Username & passwprd
     client.on_connect = on_connect      # 将回调函数指派给客户端实例
     client.on_message = on_message
-    client.on_publish = on_publish
+    #client.on_publish = on_publish
     client.on_subscribe = on_subscribe
     client.connect(broker, port, 60)    #连接MQTT服务器
+
+
+def set_Test_Logic(interval_time, run_time, record_file: str):
+    ''' 设置测试逻辑.
+        interval_time:   间隔时间S   默认60 s.\n
+        run_time:       运行时间S   默认20 s.\n
+        record_file:  记录文件路径.
+        '''
+    global INTERVAL_TIME
+    global RUN_TIME
+    INTERVAL_TIME   = interval_time
+    RUN_TIME        = run_time
+
+def test_start():
+
+    for i in range(RUN_TIME):
+        time.sleep(1)
 
 client = mqtt.Client(client_id)     #创建mqtt客户端
 
 if __name__ == '__main__':
-    #client = mqtt.Client(client_id)     #创建mqtt客户端
     clinet_Init()
-    
-    time.sleep(2)
-
     # 循环处理网络流量和消息回调
     client.loop_start()
 
     while True:
+        # 记录开始发送消息的时间戳
+        #start_time = time.time()
+        publish_message(topic, 'C6')
+
         get_car_reporting_data()
-        time.sleep(2)
+
+        time.sleep(1)
+        #break
 
     client.loop_stop()
 
--- test_gui.py
+++ test_gui.py
@@ -1,24 +1,36 @@
 import PySimpleGUI as sg
-#import mqttSend as ms
-
+import mqttSend as ms
+import time
 sg.theme('DarkAmber')   # Add a touch of color
 layout = [
     [sg.Text("终端SN:"), sg.Input(key="SN", do_not_clear=True) ],
+    [sg.Button("亮灯"), sg.Button("上报车辆数据")],
     [sg.Text("Beat:"), sg.Input(key="BEAT", do_not_clear=True)],
     [sg.Text("授权类型:"), sg.Combo(('MP3租赁', 'WAV租赁','进阶租赁'), key="-CB-",default_value='请选择',size=(20,1), enable_events=True)],
     [sg.Button("确定"), sg.Exit()]
 ]
 
-window = sg.Window("暗锁测试工具", layout,element_justification="right")
+if __name__ == '__main__':
+    window = sg.Window("暗锁测试工具", layout,element_justification="right")
+    ms.clinet_Init()
 
+    ms.client.loop_start()
+    while True:
+        event, values = window.read()
 
-while True:
-    event, values = window.read()
+        #退出按钮
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
 
-    #退出按钮
-    if event == sg.WINDOW_CLOSED or event == 'Exit':
-        break
+        if event == "确定":
+            break
 
-    if event == "确定":
-        break
-window.close()
+        if event == "亮灯":
+            ms.publish_message(ms.topic, 'C6')
+
+        if event == "上报车辆数据":
+            ms.get_car_reporting_data()
+            #time.sleep(1)
+
+    ms.client.loop_start()
+    window.close()
